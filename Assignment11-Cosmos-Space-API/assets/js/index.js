6b5d6bc01c8eb3c40317e4d3b22a7c65de07703e
NohaOmar20 <nohaomar507@gmail.com>
2026-08-15 19:46:19 +0300

    hide and show the section from asside

--- Assignment11-Cosmos-Space-API/assets/js/index.js
+++ Assignment11-Cosmos-Space-API/assets/js/index.js
@@ -1,2 +1,21 @@
+/* Controling the nav linkes in the aside bar when clicked show it and hide the other links
+To make a loop over them>> we can use the querySelectorAll , it will return to me nodeList, to select all the links
+ and then we can use the forEach method to loop over them
+ and add an event listener to each link. 
+ When a link is clicked>> use the classList property to add and remove hidden class from the links.
+*/ 
+
+var links = document.querySelectorAll('.nav-link');
+// console.log(links);
+var sections = document.querySelectorAll('.app-section');
+// console.log(sections);
+links.forEach((link)=> link.addEventListener('click', ()=>{
+    sections.forEach((section)=> section.classList.add('hidden'));
+
+    var targetSec = link.dataset.section;
+    document.getElementById(targetSec).classList.remove('hidden');
+}));
+
+
 // API Key: fIfuAkQWlCsirW6lOilX80hBK9Kiifj2FCXb7NiN
 const API_KEY = 'fIfuAkQWlCsirW6lOilX80hBK9Kiifj2FCXb7NiN';
